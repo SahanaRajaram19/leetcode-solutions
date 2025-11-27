e2104a7cdacc114fab25cf30873a9018e263820d
SahanaRajaram19 <sahanarajaram19@gmail.com>
2025-11-27 12:09:43 +0530

    Time: 1 ms (46.99%), Space: 9.6 MB (5.15%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.c
@@ -0,0 +1,14 @@
+
+   char* intToRoman(int num) {
+    char* symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    char* result = malloc(20 * sizeof(char));
+    result[0] = '\0';
+    for (int i = 0; i < 13; i++) {
+        while (num >= values[i]) {
+            strcat(result, symbols[i]);
+            num -= values[i];
+        }
+    }
+    return result;
+} 

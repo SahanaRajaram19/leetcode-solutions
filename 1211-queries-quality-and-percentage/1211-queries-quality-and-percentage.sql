d9e62e321128a69b0853d2fcb2e862f5dbace2fa
SahanaRajaram19 <sahanarajaram19@gmail.com>
2026-05-17 11:07:58 +0530

    Time: 384 ms (31.39%), Space: 0B (100%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,3 @@
+select query_name, round(avg(rating *1.00/position),2) as quality,
+round(sum(case when rating < 3 then 1 else 0 end) * 100 /count(*),2) as poor_query_percentage from queries where query_name is not null 
+group by query_name;

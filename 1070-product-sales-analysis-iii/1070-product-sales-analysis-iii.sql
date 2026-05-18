cc7ecbb051e575383d5cc8009fbe719a62ed6127
SahanaRajaram19 <sahanarajaram19@gmail.com>
2026-05-18 15:51:58 +0530

    Time: 760 ms (31.79%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,10 @@
+with first as
+(
+select product_id, min(year) first_year from sales 
+group by product_id
+)
+
+select  s.product_id,f.first_year,s.quantity, s.price from
+sales as s join first as f on
+s.product_id=f.product_id and s.year=f.first_year
+;

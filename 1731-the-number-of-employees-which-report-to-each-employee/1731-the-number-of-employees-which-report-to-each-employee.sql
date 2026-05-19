3bf27681ef1c261131737baf4f5a11cd6c042a2e
SahanaRajaram19 <sahanarajaram19@gmail.com>
2026-05-19 21:46:25 +0530

    Time: 646 ms (49.75%), Space: 0B (100%) - LeetHub

---
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,5 @@
+select e.employee_id,e.name, count(m.reports_to) as reports_count,
+round(avg(m.age)) as average_age
+from employees e join employees m on e.employee_id=m.reports_to
+group by e.employee_id
+order by e.employee_id;
